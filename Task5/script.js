08b63480b268a7a794a11e00c549e12eae65bfb5
SvetlanaMalkina <mal.svetlanushka@mail.ru>
2021-06-30 15:49:19 +0300

    Домашняя работа 3. На проверку.

--- Task5/script.js
+++ Task5/script.js
@@ -1,29 +1,38 @@
 //Домашнее задание 3
+
 //Задание 1
-/*
+
 let a=0;
 while (a<=100){
     console.log(a++)
 }
-*/
+
 //Задание 2
 
-/*let i=0
-let str='';
-for (i=1;i<=20;i++){
+let i = 0;
+let str = '';
+for (i = 1; i <= 20; i++){
     str += '*';
     console.log(str)
-}*/
+}
+
 //Задание 3
-b=0;
-for(b=0;b<=10;b++){
 
-   switch (b) {
-       case b===0:
-           console.log(b+'-'+'это ноль');
-       case b%2!==0:
-           console.log(b+'-'+'нечётное число');
-       case b%2===0:
-           console.log(b+'-'+'чётное число');
-   }
+let b = 0;
+console.log(b + '-' + 'это ноль')
+for(let b = 1; b <= 10; b++) {
+    if(b % 2 !== 0){
+        console.log(b + '-' + 'нечётное число');
+    }
+    else{
+        console.log(b + '-' + 'чётное число');
+    }
+}
 
+//Задание 4 (???)
+
+/*
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+alert()*/
