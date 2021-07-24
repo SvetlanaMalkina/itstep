b086fd799e2c88d093acbfad69f1d8ee3f788a41
SvetlanaMalkina <mal.svetlanushka@mail.ru>
2021-07-24 23:56:35 +0300

    Внесены изменения

--- Task5/script.js
+++ Task5/script.js
@@ -1,49 +1,63 @@
-//Задача 1
+//Задание 1
 
-//Написать функцию, которая принимает длину и ширину прямоугольника и вычисляет его площадь.
-// Если в функцию передали 1 параметр, то она вычисляет площадь квадрата
+class TextNews{
+    constructor(title,date,
+                text, tags) {
+        this.title=title;
+        this.date=date;
+        this.text=text;
+        this.tags=tags;
+    }
+    print (){
+        let tit = `<h1 style="font-size: 28px; color: black"> ${this.title} </h1>`;
+        let date = `${this.date}`;
+        if (date = (new Date()).getTime()){
+            date = 'Сегодня';
+        }
 
-let length = 5;
-let width = 10;
+        let text = `<p style="font-size: 18px; color: black"> ${this.text} </p>`;
+        let tag = `${this.tags}`;
+        document.write(tit, date, text, tag);
+    }
+}
 
-function areaRectangle (a,b){
-    if (a && b){
-        return a*b;
+let myObj = new TextNews ('Заголовок', '24.07.2021',
+    'Очень интересный текст про очень интересное что-то, которое вам прям просто очень надо',
+    ['#интересно', '#весело', '#прикольно'].join(' '));
+myObj.print();
+
+//Задание 2
+
+/*
+let number = prompt("Введите целое число от 0 до 999");
+function NumToObj(num)
+{
+    let arrNumber = num.split('');
+    let objNumber = {};
+    if (arrNumber.length == 3){
+        objNumber['units'] = +arrNumber[0];
+        objNumber['tens'] = +arrNumber[1];
+        objNumber['hundreds'] = +arrNumber[2];
+        return objNumber;
+    }
+    if (arrNumber.length == 2){
+        objNumber['units'] = +arrNumber[0];
+        objNumber['tens'] = +arrNumber[1];
+        objNumber['hundreds'] = 0;
+        return objNumber;
+    }
+    if (arrNumber.length == 1){
+        objNumber['units'] = +arrNumber[0];
+        objNumber['tens'] = 0;
+        objNumber['hundreds'] = 0;
+        return objNumber;
     }
     else {
-       return  a*a;
+        console.log('Введённые данные не соответствуют условию');
+        return objNumber;
     }
-}
-
-console.log(areaRectangle(length));
-
-//Задача 2
-
-//Написать функцию, которая проверяет, является ли переданное ей число совершенным.
-//Совершенное число – это число, равное сумме всех своих собственных делителей.
-
-let temp = 6;
-
-function perfectNumber (num){
-    let sum = num-1;
-    for(let i = 2; num>1;){
-        if(!(num%i)){
-            sum-=i;
-            num=num/i;
-        } else i++;
-    }
-    if(!sum)
-        return ("Число: является совершенным");
-    else
-        return (`Число: не является совершенным`);
 
 }
-
-console.log(perfectNumber(temp));
-
-
-//Задача 2
-
-//Написать функцию, которая принимает минимальное и максимальное значения для диапазона,
-//и выводит только те числа из диапазона, которые являются совершенными.
-//Используйте написанную ранее функцию, чтобы узнавать, совершенное число или нет.
+let obj = NumToObj(number);
+console.log(obj);
+*/
