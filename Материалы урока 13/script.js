b2077711a7d152be406fdb8a2cef1ad8142afa62
SvetlanaMalkina <mal.svetlanushka@mail.ru>
2021-08-08 01:24:07 +0300

    Внесены изменения

---
+++ Материалы урока 13/script.js
@@ -0,0 +1,138 @@
+/*
+//Задание 1.
+
+//ES5
+function Product (name, price){
+    this.name = name;
+    this.price = price;
+}
+Product.prototype.make25PercentDiscount = function (){
+    console.log(this.price * 0.75);
+}
+let obj = new Product('cake', 3);
+console.log(obj);
+obj.make25PercentDiscount();
+
+console.log('------------------------------------------');
+//ES6
+class Product1{
+    constructor(name1, price1) {
+        this.name = name1;
+        this.price = price1;
+    }
+    make25PercentDiscount(){
+        console.log(this.price * 0.75);
+    }
+}
+
+let obj1 = new Product1('cake', 3);
+console.log(obj1);
+obj1.make25PercentDiscount();
+
+//Задание 2
+
+//ES5
+function Post (author, text, date){
+    this.author = author;
+    this.text = text;
+    this.date = date;
+}
+Post.prototype.edit = function (newText){
+    this.text = newText;
+}
+let obj3 = new Post('ABC', "Первый текст", '03-08-2021');
+obj3.edit('Второй текст');
+console.log(obj3);
+
+function AttachedPost(author,text, date){
+    Post.call(this, author,text, date);
+    this.highlighted = false;
+}
+AttachedPost.prototype = Object.create(Post.prototype);
+AttachedPost.prototype.makeTextHighlighted = function (){
+    this.highlighted = true;
+}
+AttachedPost.prototype.constructor = AttachedPost;
+let obj4 = new AttachedPost('DEF', 'Третий текст', '31-07-2021');
+obj4.edit('Четвёртый текст');
+obj4.makeTextHighlighted();
+console.log(obj4);
+
+//ES6
+class Post1 {
+    constructor(author,text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
+    }
+    edit(newText){
+        this.text = newText;
+    }
+}
+let obj5 = new Post1('ABC', "Первый текст", '03-08-2021');
+obj5.edit('Второй текст');
+console.log(obj5);
+
+class AttachedPost1 extends Post1{
+    constructor(author,text, date) {
+        super(author,text, date);
+    }
+    fullEdit1(newText){
+        super.edit();
+    }
+    makeTextHighlighted(){
+        this.highlighted = true;
+    }
+}
+let obj6 = new AttachedPost1('DEF', 'Третий текст', '31-07-2021');
+obj6.edit('Четвёртый текст');
+obj6.makeTextHighlighted();
+console.log(obj6);
+*/
+
+//Задание 3
+
+class NewsFeed {
+    constructor(arr) {
+        this.arr = arr;
+    }
+    get getNewsCount() {
+        if (!(this.arr instanceof Array)) {
+            return null;
+        }
+        return this.arr.length;
+    }
+    print(){
+        return this.arr.join('/');
+    }
+    newPush(text){
+        return this.arr.push(text);
+    }
+    newDelete(i){
+        this.arr.splice(i,1);
+    }
+    findTag(str){
+        return this.arr.reduce((accum,item)=>{
+            let rightBracken=item.indexOf('>')
+            debugger
+            let tag=item.substring(1,rightBracken)
+            if (str.toLowerCase()===tag.toLowerCase()){
+                accum.push(item)
+            }
+            return accum
+        },[])
+    }
+}
+let newsObj = new NewsFeed([
+    '<p>Новости спорта</p>',
+    '<h2>Погода на завтра</h2>',
+    '<a data-date="25-06-2020">Происшествия за #сегодня</a>',
+    '<div>Новости политики</div>'
+])
+console.log(newsObj.print());
+newsObj.newPush('<span>Последние новости часа</span>');
+console.log(newsObj);
+//newsObj.newDelete(2);
+console.log(newsObj.findTag('div'));
+console.log(newsObj);
+console.log(newsObj.getNewsCount);
